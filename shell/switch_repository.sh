ba6395b8a53c8d2f25e1a185f57c266ab9a9d017
mineextremely <100750989+mineextremely@users.noreply.github.com>
2026-03-01 14:30:13 +0800

    Update Config

--- shell/switch_repository.sh
+++ shell/switch_repository.sh
@@ -1,6 +1,6 @@
 # 将Imagebuilder默认repositories 切换到镜像源 避免下载失败
 OFFICIAL="https://downloads.immortalwrt.org"
-MIRROR="https://mirrors.cernet.edu.cn/immortalwrt"
+MIRROR="https://mirrors.pku.edu.cn/immortalwrt"
 echo ">>> official failed, switching to mirror"
 BASE_URL="$MIRROR"
 echo "Using BASE_URL = $BASE_URL"
