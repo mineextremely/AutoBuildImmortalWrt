24034cbf01ebb582538cdce757c8ec042e717d2c
mineextremely <100750989+mineextremely@users.noreply.github.com>
2026-03-24 15:15:15 +0800

    TEST: add support for Luci 25.12

---
+++ rockchip/build25.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Log file for debugging
+source shell/switch_repository.sh
+
+LOGFILE="/tmp/uci-defaults-log.txt"
+echo "Starting rockchip build25.sh at $(date)" >> "$LOGFILE"
+
+# yml 传入的路由器型号 PROFILE
+echo "Building for profile: $PROFILE"
+# yml 传入的固件大小 ROOTFS_PARTSIZE
+echo "Building for ROOTFS_PARTSIZE: $ROOTFS_PARTSIZE"
+
+echo "Create pppoe-settings"
+mkdir -p /home/build/immortalwrt/files/etc/config
+
+# 创建 PPPoE 配置文件，供 99-custom.sh 读取
+cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
+enable_pppoe=${ENABLE_PPPOE}
+pppoe_account=${PPPOE_ACCOUNT}
+pppoe_password=${PPPOE_PASSWORD}
+EOF
+
+echo "cat pppoe-settings"
+cat /home/build/immortalwrt/files/etc/config/pppoe-settings
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建 25.12.x 实验固件..."
+echo "查看 repositories.conf 信息----------------"
+cat repositories.conf
+
+# 25.12.x 使用 apk 包管理，实验版只保留官方仓库软件包，不集成第三方 run/ipk 包
+PACKAGES=""
+PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-theme-argon"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES xray-core hysteria luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+
+if [ "$INCLUDE_DOCKER" = "yes" ]; then
+    PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+    echo "Adding package: luci-i18n-dockerman-zh-cn"
+fi
+
+# 若构建 openclash 则添加内核
+if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
+    echo "✅ 已选择 luci-app-openclash，添加 openclash core"
+    mkdir -p files/etc/openclash/core
+    META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/smart/clash-linux-arm64.tar.gz"
+    wget -qO- "$META_URL" | tar xOvz > files/etc/openclash/core/clash_meta
+    chmod +x files/etc/openclash/core/clash_meta
+else
+    echo "⚪️ 未选择 luci-app-openclash"
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
+echo "$PACKAGES"
+
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
+
+if [ $? -ne 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
+    exit 1
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
